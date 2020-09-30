e3a9fdd1fdeae56648f01a5281c6edb20a0b1777
Trevin Wisaksana <trevin@stockbit.com>
2020-09-30 19:36:48 +0700

    Welcome View is now loaded from nib

--- MyWorkoutTestProject/Components/Welcome/ViewController/WelcomeViewController.swift
+++ MyWorkoutTestProject/Components/Welcome/ViewController/WelcomeViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    // MARK: -
+    
+    lazy var welcomeView: WelcomeView = {
+        let nibName = String(describing: WelcomeView.self)
+        let bundle = Bundle(for: type(of: self))
+        let view = bundle.loadNibNamed(nibName, owner: self, options: nil)?.first as! WelcomeView
+        return view
+    }()
+    
     // MARK: - Application Lifecycle
     
     override func loadView() {
         super.loadView()
         
-        self.view = WelcomeView()
+        self.view = welcomeView
     }
 
     override func viewDidLoad() {
