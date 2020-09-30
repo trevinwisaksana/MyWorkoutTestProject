cd0711e42183e171bafd91881886c69dd6e205be
Trevin Wisaksana <trevin@stockbit.com>
2020-09-30 19:31:18 +0700

    Added the ability to launch WelcomeViewController

--- MyWorkoutTestProject/Components/Welcome/ViewController/WelcomeViewController.swift
+++ MyWorkoutTestProject/Components/Welcome/ViewController/WelcomeViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    // MARK: - Application Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.view = WelcomeView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
