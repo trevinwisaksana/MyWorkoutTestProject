801e45fc7b5058a8a7e12a61ef257ca9fa1fa908
Trevin Wisaksana <trevin@stockbit.com>
2020-10-02 08:44:21 +0700

    Created UI for profile screen

--- MyWorkoutTestProject/Components/Profile/View/ProfileView.swift
+++ MyWorkoutTestProject/Components/Profile/View/ProfileView.swift
@@ -9,6 +9,26 @@ import UIKit
 
 final class ProfileView: UIView {
     
+    // MARK: -
     
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    // MARK: -
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setButtonCornerRadius()
+    }
+    
+    private func setButtonCornerRadius() {
+        logoutButton.layer.cornerRadius = 8
+    }
+    
+    // MARK: - 
+    
+    @IBAction func didTapLogOutButton(_ sender: UIButton) {
+        
+    }
 
 }
