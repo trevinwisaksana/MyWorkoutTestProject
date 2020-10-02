727a6bfb5d0d9c814a442ac48eafe01878a7e43b
Trevin Wisaksana <trevin@stockbit.com>
2020-10-02 08:59:30 +0700

    Added ProfileView delegate and image corner radius

--- MyWorkoutTestProject/Components/Profile/Controller/ProfileViewController.swift
+++ MyWorkoutTestProject/Components/Profile/Controller/ProfileViewController.swift
@@ -25,6 +25,7 @@ final class ProfileViewController: UIViewController {
         let nibName = String(describing: ProfileView.self)
         let bundle = Bundle(for: type(of: self))
         let view = bundle.loadNibNamed(nibName, owner: self, options: nil)?.first as! ProfileView
+        view.delegate = self
         return view
     }()
     
@@ -41,4 +42,12 @@ final class ProfileViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+}
+
+// MARK: - ProfileViewDelegate
+
+extension ProfileViewController: ProfileViewDelegate {
+    func profileView(_ view: ProfileView, didTapLogOutButton button: UIButton) {
+        
+    }
 }
--- MyWorkoutTestProject/Components/Profile/View/ProfileView.swift
+++ MyWorkoutTestProject/Components/Profile/View/ProfileView.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
+protocol ProfileViewDelegate: class {
+    func profileView(_ view: ProfileView, didTapLogOutButton button: UIButton)
+}
+
 final class ProfileView: UIView {
     
     // MARK: -
     
+    weak var delegate: ProfileViewDelegate?
+    
+    // MARK: -
+    
+    @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var logoutButton: UIButton!
     
     // MARK: -
@@ -19,16 +28,23 @@ final class ProfileView: UIView {
         super.awakeFromNib()
         
         setButtonCornerRadius()
+        setProfileImageCornerRadius()
     }
     
     private func setButtonCornerRadius() {
         logoutButton.layer.cornerRadius = 8
     }
     
+    private func setProfileImageCornerRadius() {
+        let height = profileImageView.frame.height / 2
+        profileImageView.layer.cornerRadius = height
+        profileImageView.clipsToBounds = true
+    }
+    
     // MARK: - 
     
     @IBAction func didTapLogOutButton(_ sender: UIButton) {
-        
+        delegate?.profileView(self, didTapLogOutButton: sender)
     }
 
 }
